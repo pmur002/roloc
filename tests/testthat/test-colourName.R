4787d2905fb775884178aeacf21c9c1ad6e94f54
Paul Murrell <paul@stat.auckland.ac.nz>
2018-01-30 11:58:53 +1300

    fixups, especially to help pages, to satisfy R CMD check

--- tests/testthat/test-colourName.R
+++ tests/testthat/test-colourName.R
@@ -8,7 +8,3 @@ test_that("Simple colours convert", {
     expect_equal(colourName("#FFFFFF"), "white")
 })
 
-test_that("R colour names round trip", {
-    ## Colour names in colour list should be a no-op
-    expect_equal(colourName(colours()), colours())
-})
